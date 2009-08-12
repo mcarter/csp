c59464197422f315232350d144d7a59e61753edb
Jacob Rus <jacob.rus@gmail.com>
2009-08-12 16:55:53 +0000

    Added test_roundtrip to test single echo packet. (Worked with mcarter.)

--- cspacceptor/protocol.py
+++ cspacceptor/protocol.py
@@ -13,3 +13,25 @@ def handshake():
         return session_data['session'], response
     except:
         raise error.CSPException("Invalid handshake response", response)
+
+def send(session, data):
+    packet = json.dumps([[1,0, data]])
+    response = http.make_request(config.base_url + '/send?s=' + session + '&d=' + packet)
+    try:
+        # XXX: check spec and fully parse response
+        assert 'OK' in response.body
+    except:
+        raise error.CSPException("Invalid send response", response)
+    return response
+    
+def comet(session):
+    response = http.make_request(config.base_url + '/comet?s=' + session)
+    output = ""
+    try:
+        packets = json.loads(response.body[1:-1])
+        for packet in packets:
+            # XXX: use the proper decode function (urldecode, maybe)
+            output += packet[2]
+    except:
+        raise error.CSPException("Invalid comet response", response)
+    return output, response
---
+++ cspacceptor/test_roundtrip.py
@@ -0,0 +1,33 @@
+import simplejson
+import http
+from error import CSPException
+import config
+import protocol
+from eventlet import coros, api
+
+class TestRoundtrip(object):
+    base_url = config.base_url
+    
+    def test_echo_roundtrip(self):
+        session, handshake_response = protocol.handshake()
+        def comet_request(event):
+            try:
+                event.send(protocol.comet(session))
+            except Exception, e:
+                event.send_exception(e)
+        def send_request(event):
+            try:
+                event.send(protocol.send(session, "abc"))
+            except Exception, e:
+                event.send_exception(e)
+                
+        comet_event = coros.event()
+        send_event = coros.event()
+        api.spawn(comet_request, comet_event)
+        api.spawn(send_request, send_event)
+        result, comet_response = comet_event.wait()
+        send_response = send_event.wait()
+        send_response.append_transcript(comet_response.socket.transcript)
+        if result != 'abc':
+            raise CSPException("Received different data than we sent.", send_response)
+        
