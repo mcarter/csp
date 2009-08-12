fa683e9e53a82aaa0df7cb5351da26dec0d7f688
mcarter <cartermichael@gmail.com>
2009-08-12 13:41:55 +0000

    added POST /reflect test

--- cspacceptor/test_reflect.py
+++ cspacceptor/test_reflect.py
@@ -13,8 +13,20 @@ class TestReflectResponse(object):
         url = self.base_url +  "/reflect?s=" + session + "&d=" + payload
         response = http.make_request(url, reset_transcript=False, socket=handshake_response.socket)
         if response.code != 200:
-            raise CSPException("Reflect should return status code 200",
+            raise CSPException("Reflect (GET) should return status code 200",
                                response)
         if payload != response.body:
-            raise CSPException("Reflect returned invalid response body.",
+            raise CSPException("Reflect (GET) returned invalid response body.",
+                               response)
+                               
+    def test_reflect_post(self):
+        session, handshake_response = protocol.handshake()
+        payload = "<script>alert('woot')</script>"
+        url = self.base_url +  "/reflect?s=" + session + "&d=" + payload
+        response = http.make_request(url, method='POST', reset_transcript=False, socket=handshake_response.socket)
+        if response.code != 200:
+            raise CSPException("Reflect (POST) should return status code 200",
+                               response)
+        if payload != response.body:
+            raise CSPException("Reflect (POST) returned invalid response body.",
                                response)
