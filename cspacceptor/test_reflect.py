317ed92d96dfacd02f5584e99883f72e393bc99e
Jacob Rus <jacob.rus@gmail.com>
2009-09-30 06:23:27 +0000

    fix test_reflect_post to match spec updates

--- cspacceptor/test_reflect.py
+++ cspacceptor/test_reflect.py
@@ -23,8 +23,8 @@ class TestReflectResponse(object):
     def test_reflect_post(self):
         session, handshake_response = protocol.handshake()
         payload = "<script>alert('woot')</script>"
-        url = self.base_url +  "/reflect"
-        body = "s=" + session.key + "&d=" + payload
+        url = self.base_url +  "/reflect?s=" + session.key
+        body = payload
         response = http.make_request(url, method='POST', body=body, 
                     reset_transcript=False, socket=handshake_response.socket)
         if response.code != 200:
